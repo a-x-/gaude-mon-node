8afad84edea25beddcc279ce2e22dd84ed479a4f
alex mextner <Aleksandr.MEKHONOSHIN@raiffeisen.ru>
2021-03-27 21:28:47 +0300

    port to node.js
    deno does not work on rpi yet

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM hayd/alpine-deno:1.8.2
+FROM node:alpine
 
 # The port that your application listens to.
 EXPOSE 8080
@@ -6,7 +6,7 @@ EXPOSE 8080
 WORKDIR /app
 
 # Prefer not to run as root.
-USER deno
+USER node
 
 # Cache the dependencies as a layer (the following two steps are re-run only when src/deps.ts is modified).
 # Ideally cache src/deps.ts will download and compile _all_ external files used in src/index.ts.
@@ -15,7 +15,5 @@ USER deno
 
 # These steps will be re-run upon each file change in your working directory:
 ADD . .
-# Compile the src/index app so that it doesn't need to be compiled each startup/entry.
-RUN deno cache src/index.ts
 
-CMD ["run", "--allow-net", "--allow-read", "--allow-write", "src/index.ts"]
+CMD ["node", "src/index.ts"]
---
+++ src/index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+
+const { formatRelative } = require("date-fns");
+const { ru } = require("date-fns/locale");
+
+const port = 8080;
+const locale = ru;
+const paths = { template: `${__dirname}/../template.html`, store: `${__dirname}/../.store.json` };
+const store /*Data*/ = readJsonFile(paths.store) || { cld: 0, hot: 0, upd: new Date().toISOString() };
+
+app.use(express.json()); // for parsing application/json
+
+//
+//
+//
+
+app
+  //
+  .put("/roscha/api/wc/water", async (request, response) => {
+    console.log(`${request.method} ${request.url}`);
+
+    try {
+      const data /*Data*/ = request.body;
+      Object.assign(store, data);
+      try {
+        await write(paths.store, JSON.stringify(store, null, 4));
+        response.send({ status: "OK" });
+      } catch (e) {
+        const errMsg = "DIDNOT_PERSIST";
+        console.error(errMsg, e);
+        response.send({ status: errMsg });
+      }
+    } catch (e) {
+      const errStr = "Updating failed";
+      console.error(errStr, e);
+      Object.assign(response, { status: 500, body: errStr + "\n" + e.message });
+    }
+  })
+  .get("/roscha/water.html", async (request, response) => {
+    console.log(`${request.method} ${request.url}`);
+
+    const template = read(paths.template);
+    response.send(subst(template, store));
+  });
+
+//
+//
+//
+
+// app.use(async (context) => {
+//   if (context.request.method !== "GET") return;
+//   console.log("static", "GET", context.request.url.pathname);
+//   if (!context.request.url.pathname.startsWith("/roscha")) return;
+
+//   try {
+//     await send(context, context.request.url.pathname.replace("/roscha", ""), {
+//       root: `${__dirname}/../static`,
+//       index: "index.html",
+//     });
+//   } catch (e) {
+//     const errStr = "Static upload failed";
+//     console.error(errStr, e);
+//     context.response.send({ status: 500, body: errStr + "\n" + e.message });
+//   }
+// });
+
+app.use("/roscha", express.static(`${__dirname}/../static`));
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+});
+
+/*type Data = {
+  hot: number;
+  cld: number;
+  upd: string; // DateTimeIso
+}*/
+
+function subst(template, data /*Data*/) {
+  const upd = formatDate(data.upd);
+  return template.replace("{hot}", data.hot.toFixed(3)).replace("{cld}", data.cld.toFixed(3)).replace("{upd}", upd);
+}
+
+function read(name) {
+  return require("fs").readFileSync(name, "utf8");
+}
+function write(name, data) {
+  return require("fs").writeFileSync(name, data);
+}
+
+function readJsonFile(path) {
+  const json = read(path);
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    console.error(`JSON hadn't parsed: ${e}\npath: ${path}\ncontent: «${json}»`);
+    return undefined;
+  }
+}
+
+function formatDate(date) {
+  try {
+    return formatRelative(new Date(date), new Date(), { locale });
+  } catch (e) {
+    console.error("Date had not formatted", e, date);
+    return date;
+  }
+}
